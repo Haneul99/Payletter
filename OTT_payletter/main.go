cdaee10ad52ea3f63d70169d14703e188bd0cc10
Haneul99 <healing5746@gmail.com>
2022-03-17 10:50:57 +0900

    db select

--- OTT_payletter/main.go
+++ OTT_payletter/main.go
@@ -10,6 +10,13 @@ import (
 	//	"github.com/labstack/echo"
 )
 
+type ottservice struct {
+	OTTserviceId int64
+	platform     string
+	membership   string
+	price        int64
+}
+
 var db *sql.DB
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -20,11 +27,11 @@ func dbConnect() {
 	var err error
 
 	dbURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
-		util.ServerConfig.GetStringData("DB_OTTS_LoginID"),
-		util.ServerConfig.GetStringData("DB_OTTS_Password"),
-		util.ServerConfig.GetStringData("DB_OTTS_IP"),
-		util.ServerConfig.GetStringData("DB_OTTS_Port"),
-		util.ServerConfig.GetStringData("DB_OTTS_Name"))
+		util.ServerConfig.GetStringData("DB_LoginID"),
+		util.ServerConfig.GetStringData("DB_Password"),
+		util.ServerConfig.GetStringData("DB_IP"),
+		util.ServerConfig.GetStringData("DB_Port"),
+		util.ServerConfig.GetStringData("DB_Name"))
 
 	db, err = sql.Open("mysql", dbURL)
 	if err != nil {
@@ -34,6 +41,30 @@ func dbConnect() {
 	}
 }
 
+// OTTservices Table 정보 SELECT
+func getOTTservices() []ottservice {
+	query := fmt.Sprintf("SELECT * FROM %s", "ottservices")
+	fmt.Println(query)
+
+	rows, err := db.Query(query)
+	if err != nil {
+		fmt.Println(err, "db query failed")
+	}
+	defer rows.Close()
+
+	results := []ottservice{}
+
+	for rows.Next() {
+		var ott ottservice
+		err = rows.Scan(&ott.OTTserviceId, &ott.platform, &ott.membership, &ott.price)
+		if err != nil {
+			fmt.Println(err)
+		}
+		results = append(results, ott)
+	}
+	return results
+}
+
 func main() {
 	if !util.ServerConfig.LoadConfig() {
 		panic("설정파일 읽기 실패")
@@ -41,6 +72,10 @@ func main() {
 
 	//fmt.Println(util.ServerConfig.GetData())
 	dbConnect()
+
+	results := getOTTservices()
+	fmt.Println(results)
+
 	http.HandleFunc("/", index)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
