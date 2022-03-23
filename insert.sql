80d1bf7bce23415ac824cc90b0706665d7a671ee
Haneul99 <healing5746@gmail.com>
2022-03-23 17:59:58 +0900

    modified

--- insert.sql
+++ insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO ottservices(platform, membership, price) VALUES ("TVING", "프리미엄", 13900), ("TVING", "스탠다드", 10900), ("TVING", "베이직", 7900), ("NETFLIX", "프리미엄", 17000),("NETFLIX", "스탠다드", 13500),("NETFLIX", "베이직", 9500)
 INSERT INTO USER(username, password, email) VALUES ("username11", "password11", "email@mail.com");
-INSERT INTO subscribedServices(username, OTTserviceId, subscribedDate, expireDate, paymentType) VALUES ("username11", 1,"2022-03-21", "2022-04-21" ,1);
+INSERT INTO subscribedServices(username, OTTServiceId, subscribedDate, expireDate, paymentType, tid, price, pgcode) VALUES ("username111", 2, "2022-03-23", "2022-04-23", 0, "tpay_test-202203239554540", 100, "creditcard");
